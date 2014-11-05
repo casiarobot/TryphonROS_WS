17efc1a956f225f77d59399dfda2fec38bc2413f
David <saintonge.david@gmail.com>
2014-11-04 19:25:58 -0500

    modified:   src/controls/include/ctr_fuzzy.h 	modified:   src/controls/include/cube.h 	modified:   src/state/src/state_estimator_SICK.cpp

--- src/controls/include/cube.h
+++ src/controls/include/cube.h
@@ -20,23 +20,24 @@
 #define CUBE_KAIR2		2.4		// Rotational air resistance
 #define MAX_CUBE_KAIR2		4		// Maximal Rotational air resistance
 #define MOTOR_GAIN		350		// uster selon la turbine utilis tel que 1 Fx = 1N. (Tests avec capteurs de tension)
+
 #else
 #define CUBE_LENGTH		2.25		// Cube length (m)
 
 #define MIN_CUBE_MASS		0		// Minimal cube mass (kg)
-#define CUBE_MASS			3.5		// Estimated cube mass (kg)
-#define MAX_CUBE_MASS		5		// Maximal cube mass (kg)
+#define CUBE_MASS		11		// Estimated cube mass (kg)
+#define MAX_CUBE_MASS		13		// Maximal cube mass (kg)
 
 #define MIN_CUBE_INERTIA	0		// Min Cube inertia (kg*m^2)
-#define CUBE_INERTIA		16		// Cube inertia (kg*m^2)
-#define MAX_CUBE_INERTIA	30		// Max Cube inertia (kg*m^2)  //ATTENTION TROP GRAND??? = agressif
+#define CUBE_INERTIA		14		// Cube inertia (kg*m^2)
+#define MAX_CUBE_INERTIA	20		// Max Cube inertia (kg*m^2)  //ATTENTION TROP GRAND??? = agressif
 
 #define MIN_CUBE_KAIR1		0		// Minimal Lateral air resistance
-#define CUBE_KAIR1			9.5		// Estimated Lateral air resistance (prop. < la surf.)
+#define CUBE_KAIR1		9.5		// Estimated Lateral air resistance (prop. < la surf.)
 #define MAX_CUBE_KAIR1		16		// Maximal Lateral air resistance
 
 #define MIN_CUBE_KAIR2		0		// Minimal Rotational air resistance
-#define CUBE_KAIR2			9.5		// Rotational air resistance
+#define CUBE_KAIR2		9.5		// Rotational air resistance
 #define MAX_CUBE_KAIR2		16		// Maximal Rotational air resistance
 #define MOTOR_GAIN		255		// uster selon la turbine utilis el que 1 Fx = 1N. (Tests avec capteurs de tension)
 #endif
@@ -56,11 +57,11 @@
 
 // luminosite
 #define	LUMINOSITE_MAX				255		// 
-#define	LUMINOSITE_STABILISATION	180		// 
+#define	LUMINOSITE_STABILISATION		180		// 
 #define	LUMINOSITE_OBJET			50		// unites
-#define LUMINOSITE_INTERACTION		0 //80		//0 Pas dinteraction, negatif attire, positif effraye
+#define LUMINOSITE_INTERACTION			0 //80		//0 Pas dinteraction, negatif attire, positif effraye
 #define LUMINOSITE_DISTANCE			150		//DISTANCE = LUMINOSITE/LUMINOSITE_DISTANCE
 
 // orientation
 #define	ANGLE_INDEFINI				-360.1F		// degres
-#define	TEMPERATURE_INDEFINIE		100.1F		// degres
+#define	TEMPERATURE_INDEFINIE			100.1F		// degres
