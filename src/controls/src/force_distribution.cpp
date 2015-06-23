e32b05b566b013ba937e3ffbda7586a9c179cf6a
tryphon.cube <tryphon.cube@outlook.com>
2015-06-23 11:36:31 -0400

    Added launch files for force dist and got it working with gazebo

--- src/controls/src/force_distribution.cpp
+++ src/controls/src/force_distribution.cpp
@@ -221,55 +221,14 @@ void mySigintHandler(int sig)
   ros::shutdown();
 }
 
-const char* get_ip()
-{
-  int fd;
-  struct ifreq ifr;
-  char *ip = new char[100];
-
-  fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-  /* I want to get an IPv4 IP address */
-  ifr.ifr_addr.sa_family = AF_INET;
-
-  /* I want IP address attached to "eth0" */
-  strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
-
-  ioctl(fd, SIOCGIFADDR, &ifr);
-
-  close(fd);
-
-  /* display result */
-  sprintf(ip,"%s", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-  std::string s = ip;
-  std::replace(s.begin(), s.end(), '.', '_');
-  //ip=s.c_str();
-  return s.c_str();
-}
-
 int main(int argc, char **argv)
 {
 
-  char rosname[100],ip[100];
-  sprintf(rosname,"force_estimation_%s",get_ip());
-  std::string s, temp_arg ;
+ 
   signal(SIGINT, mySigintHandler);
 
-  ros::init(argc, argv, rosname);
+  ros::init(argc, argv, "force_distribution");
   ros::NodeHandle node;
-
-  if (argc==2)
-  {
-    ROS_INFO("TARGET IS: %s", argv[1]);
-  }
-  else
-  {
-    ROS_ERROR("Failed to get param 'target'");
-    return 0;
-  }
-
-  temp_arg = argv[1];
-  std::replace(temp_arg.begin(), temp_arg.end(), '.', '_');
 
   dynamic_reconfigure::Server<controls::fdistributionConfig> server;
   dynamic_reconfigure::Server<controls::fdistributionConfig>::CallbackType f;
@@ -278,20 +237,15 @@ int main(int argc, char **argv)
   server.setCallback(f);
 
   // Publishers //
-  sprintf(rosname,"/%s/command_props",temp_arg.c_str());
-  props_node = node.advertise<tests::props_command>(rosname,1);
+  props_node = node.advertise<tests::props_command>("command_props",1);
 
 
   // Subscribers //
-  sprintf(rosname,"/%s/max_thrust",temp_arg.c_str());
-  ros::Subscriber subM = node.subscribe(rosname, 1, subMax);
-  sprintf(rosname,"/%s/command_control",temp_arg.c_str());
-  ros::Subscriber subF = node.subscribe(rosname, 1, subForces);
-  sprintf(rosname,"/%s/intFz_control",temp_arg.c_str());
-  ros::Subscriber subFz = node.subscribe(rosname, 1, subForcez);
-
-  sprintf(rosname,"/%s/motors_info",temp_arg.c_str());
-  ros::Subscriber subMI = node.subscribe(rosname, 1, subMotor);
+ 
+  ros::Subscriber subM = node.subscribe("max_thrust", 1, subMax);
+  ros::Subscriber subF = node.subscribe("command_control", 1, subForces);
+  ros::Subscriber subFz = node.subscribe("intFz_control", 1, subForcez);
+  ros::Subscriber subMI = node.subscribe("motors_info", 1, subMotor);
 
   // Variables //
   double vP[12][3], vPraw[12][3], vPz[12][3], vPzraw[12][3], commands[12][3];  // only the last row of commands will be used but it is made so the function scale can be used.
